a1068083b8d37f21446292691085906a2870b2ae
RogueStar112 <hildtgunnt@gmail.com>
2023-10-06 07:31:37 +0100

    Attempted to make chart history previews

--- getChartsFromApp.js
+++ getChartsFromApp.js
@@ -1,19 +1,20 @@
 const parseProductData = JSON.parse(localStorage.getItem("products"));
-const chartHistoryContainer = document.getElementById('chart-history-container');
+const chartHistoryContainer = document.getElementById(
+  "chart-history-container"
+);
 
 let graphLabels = [];
 let graphData_votes = [];
 let graphData_shown = [];
-
 
 let imageListSorted = [...parseProductData];
 
 let pastCharts = [];
 
 if (localStorage.getItem("pastCharts")) {
-  console.log("Past charts found!")
+  console.log("Past charts found!");
   pastCharts = JSON.parse(localStorage.getItem("pastCharts")).reverse();
-} 
+}
 
 // credit to: https://byby.dev/js-sort-by-object-property
 function compareByVotes(a, b) {
@@ -40,8 +41,7 @@ for (i = 0; i < imageListSorted.length; i++) {
   }
 }
 
-const ctx = document.getElementById('chart');
-
+const ctx = document.getElementById("chart");
 
 let config = new Chart(ctx, {
   type: "bar",
@@ -73,41 +73,107 @@ let config = new Chart(ctx, {
   },
 });
 
-function replaceChart(chart_id) {
-
-}
+function replaceChart(chart_id) {}
 
 function renderChartHistory() {
-
   sortData();
 
-  for (let i=0; i<pastCharts.length; i++) {
-    let div = document.createElement('div');
+  for (let i = 0; i < pastCharts.length; i++) {
+    let div = document.createElement("div");
     div.classList.add("chart-history-cell");
 
-    div.setAttribute("id", `pastChart-${i}`);    
+    div.setAttribute("id", `pastChart-${i}`);
     chartHistoryContainer.appendChild(div);
 
     div_chartDescriptor = document.createElement("div");
     div_chartDescriptor.classList.add("chart-descriptor");
 
-    div_chartDescriptor.textContent = `Chart ${i+1}`
+    div_chartDescriptor.textContent = `Chart ${i + 1}`;
+
+    // miniCanvas = document.createElement("canvas");
+    // miniCanvas.setAttribute("id", `miniCanvas-${i}`);
 
     div.appendChild(div_chartDescriptor);
+    // div.appendChild(miniCanvas);
+
+    // ctxMini = document.getElementById(`miniCanvas-${i}`);
 
     let currentChart = pastCharts[i];
-    
+
     console.log("currentChart", currentChart);
 
-    div.addEventListener("click", function() {
-      replaceChart(currentChart)
-    })
+    div.addEventListener("click", function () {
+      replaceChart(currentChart);
+    });
     // for (let x=0; x<pastCharts[i].length; x++) {
 
-      
     //     // console.log(pastCharts[i][x]);
 
     // }
+
+    let miniCtx_votes = [];
+    let miniCtx_shown = [];
+
+    // for (let i = 0; i < currentChart.length; i++) {
+    //   if (currentChart[i].times_voted != 0) {
+    //     miniCtx_votes.push(currentChart[i].times_shown);
+    //     miniCtx_shown.push(currentChart[i].times_voted);
+    //   }
+    // }
+
+    // config = new Chart(ctxMini, {
+    //   type: "bar",
+    //   data: {
+    //     labels: graphLabels,
+    //     datasets: [
+    //       {
+    //         type: "bar",
+    //         label: "# of votes",
+    //         data: miniCtx_votes,
+    //         // borderWidth: 6,
+    //         backgroundColor: ["red", "green", "skyblue", "purple", "orange"],
+    //       },
+    //       {
+    //         type: "bar",
+    //         label: "# of times shown",
+    //         data: miniCtx_shown,
+    //         // borderWidth: 6,
+    //         backgroundColor: ["lightgrey"],
+    //       },
+    //     ],
+    //   },
+    //   options: {
+    //     scales: {
+    //       x: {
+    //         stacked: true,
+    //         display: false,
+    //       },
+    //       y: {
+    //         display: false,
+    //       },
+    //     },
+    //     responsive: false,
+
+    //     bezierCurve: false, //remove curves from your plot
+    //     scaleShowLabels: false, //remove labels
+    //     tooltipEvents: [], //remove trigger from tooltips so they will'nt be show
+    //     pointDot: false, //remove the points markers
+    //     scaleShowGridLines: true, //set to false to remove the grids background
+
+    //     plugins: {
+    //       legend: {
+    //         display: false,
+    //       },
+    //     },
+    //   },
+    // });
+
+    // let ctxURI = ctxMini.toDataURL();
+
+    // let ctxImage = new Image();
+    // ctxImage.src = ctxURI;
+
+    // ctxMini.appendChild(ctxImage);
   }
 }
 
@@ -115,16 +181,13 @@ renderChartHistory();
 
 function replaceChart(chart) {
   console.log(chart);
-  console.log('POTAOEPUJWOPAOPJ')
 
   config.destroy();
 
-  let newDataset_shown = []
-  let newDataset_votes = []
+  let newDataset_shown = [];
+  let newDataset_votes = [];
 
-
-
-  for(let i = 0; i<chart.length; i++) {
+  for (let i = 0; i < chart.length; i++) {
     newDataset_shown.push(chart[i].times_shown);
     newDataset_votes.push(chart[i].times_voted);
   }
@@ -159,3 +222,5 @@ function replaceChart(chart) {
     },
   });
 }
+
+// credit to: https://stackoverflow.com/questions/28530535/is-there-any-way-to-create-mini-very-small-chart-with-chart-js
